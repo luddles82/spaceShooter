a4d3e40800441d2a8766e4369d501a31fea54f69
Nick Ludlow <nickludlow82@gmail.com>
2015-08-16 09:45:17 +0100

    Initial Commit

---
+++ spaceShooterTests/spaceShooterTests.swift
@@ -0,0 +1,36 @@
+//
+//  spaceShooterTests.swift
+//  spaceShooterTests
+//
+//  Created by Nick Ludlow on 16/08/2015.
+//  Copyright (c) 2015 Nick Ludlow. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class spaceShooterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
